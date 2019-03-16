7e3ec1a141a598b4a78cef8f3db17d1060a288f9
kim <smile@itjoy.org>
2019-03-16 23:21:46 +0800

    add import.sh

---
+++ import.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Copy common code snippets into xcode
+# 2019/03/16
+
+SPATH='/Users/'$USER'/Library/Developer/Xcode/UserData/CodeSnippets/'
+
+cd objc
+
+for i in $(ls); do
+	if [[ ${i##*.} == 'codesnippet' ]]; then
+		cp $(pwd)/$i $SPATH
+	fi
+done
+
+echo "copy success 😄"
